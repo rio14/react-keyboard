8a4dfee4555b9bbeaf001784a0c8bddaa553df3b
rio14 <riteshsinha14.rs@gmail.com>
2021-11-28 14:53:05 +0530

    Assignment complete

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { KeypressProvider } from "./context/KeypressProvider";
+import A from "./components/A";
+import B from "./components/B";
+import C from "./components/C";
+import Listeners from "./components/Listeners";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <KeypressProvider>
+      <div className="container">
+        <A />
+        <B />
+        <C />
+        <Listeners />
+      </div>
+    </KeypressProvider>
   );
 }
 
